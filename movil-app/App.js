2c183f504b5910d9e8f55d5047544b0521b2db55
garber <garbfbatista@gmail.com>
2023-02-15 20:28:40 -0400

    menu

--- movil-app/App.js
+++ movil-app/App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+//navigation
+import { NavigationContainer } from "@react-navigation/native";
+//react native stack
+import { createStackNavigator } from "@react-navigation/stack";
+import ResumenPedido from "./view/ResumenPedido";
+import NuevaOrden from "./view/NuevaOrden";
+import DetallePlatillo from "./view/DetallePlatillo";
+import Menu from "./view/Menu";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Menu'
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#FFDA00",
+            },
+            headerTintColor: "#000",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen
+            name='Menu'
+            component={Menu}
+            options={{ title: "Menu" }}
+          />
+          <Stack.Screen
+            name='DetallePlatillo'
+            component={DetallePlatillo}
+            options={{ title: "Detalle Platillo" }}
+          />
+          <Stack.Screen
+            name='NuevaOrden'
+            component={NuevaOrden}
+            options={{ title: "Nueva Orden" }}
+          />
+          <Stack.Screen
+            name='ResumenPedido'
+            component={ResumenPedido}
+            options={{ title: "Resumen Pedido" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ movil-app/view/Menu.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native";
+const Menu = () => {
+  return <Text>Menu</Text>;
+};
+
+export default Menu;
